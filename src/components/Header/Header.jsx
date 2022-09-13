6445d97c46a8945e0b9f5b4cad893f4021eb96f6
Lakshmi Niranjan <88361371+lakshminiranjan@users.noreply.github.com>
2022-09-13 09:41:11 +0530

    firstcommit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
         <Navbar />
         <div className='container'>
             <div className='header-content text-center flex flex-column'>
-                <h1 className='text-uppercase header-title'>marketing solution agency</h1>
+                <h1 className='text-uppercase header-title'>AMEDAUS MARKETING SOLUTION AGENCY</h1>
                 <p className='text-lead'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste doloremque dolore quia omnis maxime corporis deserunt neque voluptatum.</p>
                 <a href = "/" className='btn header-btn btn-blue'>
                     <FaPaperPlane /> <span>get started</span>
