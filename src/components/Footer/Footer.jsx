4216d2b081319ebd40e618053aec3dad05e3e352
Lakshmi Niranjan <88361371+lakshminiranjan@users.noreply.github.com>
2022-09-13 15:15:01 +0530

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
     <footer className='footer bg-black flex flex-center' id = "footer">
         <div className='container flex flex-center w-100'>
             <div className='grid footer-content text-center'>
-                <p className='text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, reprehenderit!</p>
+                <p className='text'>Created by PUCCHAGINJALA LAKSHMI NIRANJAN from VELLORE INSTITUTE OF TECHNOLOGY(Vellore).</p>
                 <span className='text'>Copyright &copy; {date.getFullYear().toString()} AMEDAUS Theme</span>
             </div>
         </div>
